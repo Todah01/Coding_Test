5cc60824c28c4c7df05ec5fc9e45e532938dc67c
Tony <wobo5695@gmail.com>
2022-05-12 16:47:19 +0900

    2022.05.12_practice_1

--- Coding_Test/Test_0.cpp
+++ Coding_Test/Test_0.cpp
@@ -1,12 +1,10 @@
 //1번
-#if 1
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#if 0
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+#pragma warning(disable : 4996)
 
 using namespace std;
 
@@ -17,57 +15,173 @@ bool cmp(const string &a, const string &b)
 
 string solution(vector<int> numbers)
 {
-	string result = "";
+	string answer = "";
 	vector<string> numbers_str;
 
-	//입력
 	for (int i = 0; i < numbers.size(); i++)
 	{
 		numbers_str.push_back(to_string(numbers[i]));
 	}
-	
-	//조건부 정렬
+
 	sort(numbers_str.begin(), numbers_str.end(), cmp);
-
-	//값 설정
-	for (int i = 0; i < numbers_str.size(); i++)
-	{
-		result += numbers_str[i];
-	}
-
-	//예외처리
 	if (numbers_str[0].compare("0") == 0) return "0";
 
-	//출력
-	char* answer = (char*)malloc(sizeof(result));
-	answer = (char*)result.c_str();
+	for (int i = 0; i < numbers_str.size(); i++)
+	{
+		answer += numbers_str[i];
+	}
 	return answer;
 }
 
 int main()
 {
-	vector<int> numbers = { 6, 10, 2 };
-	
-	cout << solution(numbers);
+	int n, size;
+
+	while (1)
+	{
+		printf("숫자를 입력하세요 | 0 - 종료 | 1 - 입력 | : ");
+		scanf("%d", &n);
+		if (n == 0) break;
+		else
+		{
+			printf("SIZE 입력 : ");
+			scanf("%d", &size);
+			vector<int> numbers(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				cin >> numbers[i];
+			}
+			cout << solution(numbers) << endl;
+		}
+	}
 }
 #endif
 
-//2번
+//2번 - 1
 #if 0
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cmath>
+#include <set>
 
 using namespace std;
+set<int> number_set;
+
+bool isPrime(int number)
+{
+	if (number == 0 || number == 1)
+		return false;
+
+	for (int i = 2; i < sqrt(number); i++)
+	{
+		if (number % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+void Combination(string element, string others)
+{
+	if (element != "")
+		number_set.insert(stoi(element));
+
+	for (int i = 0; i < others.size(); i++)
+	{
+		//cout << "Combination(" + element + others[i] +","
+		//	+ others.substr(0, i) + others.substr(i + 1, others.size()) + ")" << endl;
+
+		Combination(element + others[i], others.substr(0, i) + others.substr(i + 1, others.size()));
+	}
+}
 
 int solution(string numbers)
 {
+	Combination("", numbers);
+
 	int answer = 0;
+	//for (set<int>::iterator iter = number_set.begin(); iter != number_set.end(); iter++)
+	for(int number : number_set)
+		if (isPrime(number))
+			answer++;
+
 	return answer;
 }
 
 int main()
 {
-	//cout << solution();
+	string numbers;
+	cin >> numbers;
+	cout << solution(numbers);
+}
+#endif
+
+//2번 -2
+#if 0
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+bool isPrime(int number)
+{
+	if (number == 0 || number == 1)
+		return false;
+
+	for (int i = 2; i < sqrt(number); i++)
+	{
+		if (number % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int solution(string numbers)
+{
+	int answer = 0;
+	vector<char> numbers_char;
+	vector<int> numbers_set;
+
+	for (int i = 0; i < numbers.length(); i++)
+	{
+		numbers_char.push_back(numbers[i]);
+	}
+
+	sort(numbers_char.begin(), numbers_char.end());
+
+	do {
+		string temp = "";
+		for (int i = 0; i < numbers_char.size(); i++)
+		{
+			temp.push_back(numbers_char[i]);
+			numbers_set.push_back(stoi(temp));
+		}
+	} while (next_permutation(numbers_char.begin(), numbers_char.end()));
+
+	sort(numbers_set.begin(), numbers_set.end());
+	numbers_set.erase(unique(numbers_set.begin(), numbers_set.end()), numbers_set.end());
+
+	for (int i = 0; i < numbers_set.size(); i++)
+	{
+		if (isPrime(numbers_set[i]))
+			answer++;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	string numbers;
+	cin >> numbers;
+	cout << solution(numbers);
 }
 #endif
---
+++ Coding_Test/practice_1.cpp
@@ -0,0 +1,34 @@
+#if 1
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+string solution(int n)
+{
+	string answer = "";
+	int rem;
+	while (n > 0)
+	{
+		rem = n % 3;
+		n = n / 3;
+
+		if (rem == 0) n -= 1;
+
+		answer += "412"[rem];
+	}
+	reverse(answer.begin(), answer.end());
+	return answer;
+}
+
+int main()
+{
+	
+	int n;
+	cin >> n;
+	cout << solution(n);
+}
+#endif
