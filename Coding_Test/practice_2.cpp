86849b9262d1071093c6868662c4748dcc33c3eb
Tony <wobo5695@gmail.com>
2022-05-13 14:35:52 +0900

    2022.05.13_practice_2

---
+++ Coding_Test/practice_2.cpp
@@ -0,0 +1,46 @@
+#if 1
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    int cnt = 0;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            if (prices[i] <= prices[j])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt++;
+                break;
+            }
+        }
+        answer.push_back(cnt);
+        cnt = 0;
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    vector<int> prices = { 1,2,3,2,3 };
+    vector<int> answer;
+    vector<int>::iterator iter;
+
+    answer = solution(prices);
+
+    for (iter = answer.begin(); iter != answer.end(); iter++)
+    {
+        cout << *iter << endl;
+    }
+}
+#endif
