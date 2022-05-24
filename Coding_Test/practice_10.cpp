5fdf871ce40af6caf3784ad2737e6ac2d733c988
Tony <wobo5695@gmail.com>
2022-05-24 22:09:09 +0900

    2022.05.24_practice_10

---
+++ Coding_Test/practice_10.cpp
@@ -0,0 +1,95 @@
+#if 0
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    int cnt = 0;
+    int i = 0;
+    vector<int> under_K;
+
+    //예외 처리 1 : 이미 모든 음식의 스코빌 지수가 7 이상인 경우
+    for (auto& iter : scoville)
+    {
+        if (iter < K) cnt++;
+    }
+    if (cnt == 0) return answer;
+
+    //오름차순 정렬
+    sort(scoville.begin(), scoville.end());
+
+    //수행 과정
+    while (1)
+    {
+        int new_scoville = 0;
+
+        if (i > scoville.size()) break;
+        else if (i > 0)
+        {
+            if (under_K.back() >= K) break;
+        }
+
+        if (i == 0)
+        {
+            new_scoville = scoville[0] + (scoville[1] * 2);
+            under_K.push_back(new_scoville);
+            answer++;
+        }
+        else if (i + 1 < scoville.size())
+        {
+            if (under_K[i - 1] < scoville[i + 1])
+            {
+                new_scoville = under_K[i - 1] + (scoville[i + 1] * 2);
+            }
+            else
+            {
+                new_scoville = scoville[i + 1] + (under_K[i - 1] * 2);
+            }
+            under_K.push_back(new_scoville);
+            answer++;
+        }
+        i++;
+    }
+
+    //예외 처리 1 : 모든 음식을 다 섞었음에도 스코빌 지수가 7 미만인 경우
+    if (answer == 0) return -1;
+    else return answer;
+}
+
+int main()
+{
+    vector<int> scoville = { 1,2,3,9,10,12 };
+    int K = 7;
+
+    //solution(scoville, K);
+
+    cout << solution(scoville, K);
+}
+#endif
+
+#if 1
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> scoville = { 1,2,3,9,10,12 };
+    int K = 7;
+
+    //cout << solution(scoville, K);
+}
+#endif
--- Coding_Test/practice_9.cpp
+++ Coding_Test/practice_9.cpp
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 #include <string>
 #include <vector>
 #include <map>
@@ -9,17 +9,19 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     map<string, int> map_clothes;
-   
 
+    // 1. 옷의 종류 별 갯수를 체크
     for (auto& clothInfo : clothes)
     {
         map_clothes[clothInfo[1]] += 1;
         //cout << clothInfo[1] << map_clothes[clothInfo[1]] << endl;
     }
 
+    // 2. 옷을 입지 않는 경우를 포함한 모든 경우의 수를 구함.
+    // N과 M을 하나씩 선택하는 모든 경우의 수를 구하면 N의갯수*M의갯수의 경우의 수가 나온다.
     for (auto& iter : map_clothes)
     {
-        cout << iter.second << endl;
+        //cout << iter.second << endl;
         answer *= (iter.second + 1);
     }
 
