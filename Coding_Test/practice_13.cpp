34b9d76a3237ee21e1594f2d115dda3b2d6b40db
Tony <wobo5695@gmail.com>
2022-05-31 14:36:50 +0900

    2022.05.31_practice_13

--- Coding_Test/practice_13.cpp
+++ Coding_Test/practice_13.cpp
@@ -1,14 +1,26 @@
-#if 0
-#include <iostream>
+#if 1
+#include<iostream>
 #include<vector>
+#include<algorithm>
+
 using namespace std;
+
+int comp(int a, int b)
+{
+    return a > b;
+}
 
 int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), comp);
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        answer += A[i] * B[i];
+    }
 
     return answer;
 }
