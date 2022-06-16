ac41c74b5eb4803b1c1b8f5f1d5cb664c150d96c
Tony <wobo5695@gmail.com>
2022-06-16 22:43:13 +0900

    2022.06.16_practice_23

--- Coding_Test/Test_6.cpp
+++ Coding_Test/Test_6.cpp
@@ -1,4 +1,4 @@
-//1¹ø
+//1¹ø - 1
 #if 0
 #include <string>
 #include <vector>
@@ -7,6 +7,60 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
+    int cnt = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            answer += ' ';
+            cnt = 0;
+        }
+        else if (cnt % 2 == 1)
+        {
+            answer += tolower(s[i]);
+            cnt++;
+        }
+        else
+        {
+            answer += toupper(s[i]);
+            cnt++;
+        }
+    }
+
+    return answer;
+}
+#endif
+
+//1¹ø - 2
+#if 0
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    string stringBuffer;
+    istringstream ss(s);
+    vector<string> temp;
+
+    while (getline(ss, stringBuffer, ' ')) {
+        temp.push_back(stringBuffer);
+    }
+
+    for (auto iter : temp)
+    {
+        for (int i = 0; i < iter.size(); i++)
+        {
+            if (i % 2 == 1) answer += tolower(iter[i]);
+            else answer += toupper(iter[i]);
+        }
+        answer += ' ';
+    }
+
     return answer;
 }
 #endif
@@ -15,12 +69,52 @@ string solution(string s) {
 #if 0
 #include <string>
 #include <vector>
+#include <stack>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
+
+    stack<int> basket;
+    vector<stack<int>> vertical_board(board.size());
+
+    for (int i = board.size() - 1; i >= 0; i--)
+        for (int j = board[i].size() - 1; j >= 0; j--)
+            if (board[i][j] != 0) vertical_board[j].push(board[i][j]);
+
+    // for(int i=0; i<vertical_board.size(); i++)
+    // {
+    //     while(!vertical_board[i].empty())
+    //     {
+    //         cout << vertical_board[i].top();
+    //         vertical_board[i].pop();
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < moves.size(); i++)
+    {
+        if (!vertical_board[moves[i] - 1].empty())
+        {
+            if (!basket.empty() && basket.top() == vertical_board[moves[i] - 1].top())
+            {
+                basket.pop();
+                vertical_board[moves[i] - 1].pop();
+                answer += 2;
+            }
+            else
+            {
+                basket.push(vertical_board[moves[i] - 1].top());
+                vertical_board[moves[i] - 1].pop();
+            }
+        }
+        //cout << basket.top();
+    }
+
     return answer;
+}
 }
 #endif
 
@@ -30,6 +124,7 @@ using namespace std;
 
 long long solution(int w, int h) {
     long long answer = 1;
+
     return answer;
 }
 #endif
