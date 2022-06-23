33a664c29827dcd026c8e5c05a3f07cfa11962f7
Tony <wobo5695@gmail.com>
2022-06-23 22:13:28 +0900

    2022.06.23_practice_27

--- Coding_Test/Test_7.cpp
+++ Coding_Test/Test_7.cpp
@@ -28,11 +28,48 @@ vector<string> solution(vector<string> strings, int n) {
 //2¹ø
 #if 0
 #include <string>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int solution(string dartResult) {
     int answer = 0;
+    int idx = 0;
+    int score[3] = { 0, };
+
+    for (int i = 0; i < dartResult.size(); i++)
+    {
+        if (dartResult[i] >= '0' && dartResult[i] <= '9')
+        {
+            if (dartResult[i - 1] == '1' && dartResult[i] == '0')
+            {
+                score[idx - 1] = 10;
+            }
+            else
+            {
+                score[idx] = dartResult[i] - '0';
+                idx++;
+            }
+        }
+
+        if (dartResult[i] == 'S') score[idx - 1] = pow(score[idx - 1], 1);
+        else if (dartResult[i] == 'D') score[idx - 1] = pow(score[idx - 1], 2);
+        else if (dartResult[i] == 'T') score[idx - 1] = pow(score[idx - 1], 3);
+
+        if (dartResult[i] == '#') score[idx - 1] *= -1;
+        else if (dartResult[i] == '*')
+        {
+            if (idx == 0) score[idx - 1] *= 2;
+            else
+            {
+                score[idx - 2] *= 2;
+                score[idx - 1] *= 2;
+            }
+        }
+    }
+
+    for (int iter : score) answer += iter;
 
     return answer;
 }
