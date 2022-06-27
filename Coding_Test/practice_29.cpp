a557df71653288cf15e49e18c149fbab6662eeee
Tony <wobo5695@gmail.com>
2022-06-27 21:16:40 +0900

    2022.06.27_practice_29

---
+++ Coding_Test/practice_29.cpp
@@ -0,0 +1,12 @@
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = -1;
+
+    return answer;
+}
+#endif
