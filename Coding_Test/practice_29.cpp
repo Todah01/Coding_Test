6142bb387d5939236c5f1246dfca3dfaf6107a54
Tony <wobo5695@gmail.com>
2022-07-09 18:16:20 +0900

    2022.07.09_skill_tree

--- Coding_Test/practice_29.cpp
+++ Coding_Test/practice_29.cpp
@@ -1,12 +1,42 @@
 #if 0
 #include <string>
 #include <vector>
+#include <map>
+#include <iostream>
 
 using namespace std;
 
 int solution(string skill, vector<string> skill_trees) {
-    int answer = -1;
+    int answer = 0;
+    map<char, int> tree;
 
+    for (int i = 0; i < skill.size(); i++) tree.insert({ skill[i], i + 1 });
+    // for(auto iter : tree) cout << iter.first << " : " << iter.second << endl;
+
+    for (auto skill_tree : skill_trees)
+    {
+        int cnt = 1;
+        bool check = true;
+
+        for (int i = 0; i < skill_tree.size(); i++)
+        {
+            if (tree[skill_tree[i]] > cnt)
+            {
+                // cout << "false " << tree[skill_tree[i]] << " vs " << cnt << " ";
+                cnt = 0;
+                check = false;
+                break;
+            }
+            else if (tree[skill_tree[i]] > 0)
+            {
+                // cout << "true " << tree[skill_tree[i]] << " vs " << cnt << " ";
+                cnt++;
+            }
+        }
+        cout << endl;
+
+        if (check) answer++;
+    }
     return answer;
 }
 #endif
--- Coding_Test/practice_30.cpp
+++ Coding_Test/practice_30.cpp
@@ -1,8 +1,26 @@
 #if 0
-#include <stdio.h>
-#include <string.h>
+#include <string>
 #include <vector>
+#include <iostream>
 
+using namespace std;
 
+bool solution(string S){
+	bool answer = false;
+	if (S.size() == 4 || S.size() == 6)
+	{
+		for (auto iter : S)
+		{
+			if (!(iter >= '0' && iter <= '9'))
+				answer = false;
+			else answer = true;
+		}
+	}
+	return answer;
+}
 
+int main() {
+	string S = "1234";
+	cout << solution(S);
+}
 #endif
