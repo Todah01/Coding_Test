da6fa7b0c516564eecaaaeb5940ec70c72b43406
Tony <wobo5695@gmail.com>
2022-07-20 16:56:33 +0900

    2022.07.20_Test_11

--- Coding_Test/fade.cpp
+++ Coding_Test/fade.cpp
@@ -1,11 +1,27 @@
 #if 0
 #include <string>
 #include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 0;
-    return answer;
+    int answer = 1;
+    map<string, int> clothes_map;
+
+    for (auto clothe : clothes)
+    {
+        clothes_map[clothe[1]] += 1;
+    }
+    cout << clothes_map.size();
+
+    for (auto iter : clothes_map)
+    {
+        answer *= iter.second + 1;
+    }
+
+    return --answer;
 }
 #endif
