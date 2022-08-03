5feff19d391acccb6b7be85715b0cf75603f1c3a
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-08-03 16:40:41 +0900

    2022.08.03_Test_13

---
+++ Coding_Test/Test_13.cpp
@@ -0,0 +1,27 @@
+// 1¹ø
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int C, int B) {
+    int answer = 0;
+
+    return answer;
+}
+#endif
+
+// 2¹ø
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int K, vector<int> chess_map, vector<int> start_horse) {
+    int answer = 1;
+
+    return answer;
+}
+#endif
