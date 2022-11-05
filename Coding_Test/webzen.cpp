11de02547c39742cb8a95e02f3fcad1cf9491685
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-11-05 17:58:22 +0900

    2022.11.05_remind

--- Coding_Test/webzen.cpp
+++ Coding_Test/webzen.cpp
@@ -7,32 +7,55 @@ using namespace std;
 
 int main()
 {
+	//vector<int> v = { 1,2,5,3,4 };
+	//auto it = v.begin();
+	//auto end = v.end();
+
+	//int MASIC_NUMBER = 5;
+
+	//while (it != end)
+	//{
+	//	if (*it == MASIC_NUMBER)
+	//	{
+	//		
+	//		v.erase(it++);
+	//	}
+	//	else
+	//	{
+	//		++it;
+	//	}
+
+	//	for (int idx = 0; idx < v.size(); idx++)
+	//	{
+	//		printf("%d", v[idx]);
+	//	}
+	//	printf("\n");
+	//}
+
 	vector<int> v = { 1,2,5,3,4 };
 	auto it = v.begin();
 	auto end = v.end();
 
-	printf("%p\n", &it);
-	printf("%p\n", &end);
-
 	int MASIC_NUMBER = 5;
 
-	while (it != end)
+	while (it != v.end())
 	{
+		// 홀수 제거
 		if (*it == MASIC_NUMBER)
 		{
-			
-			v.erase(it++);
+			// `erase()`는 반복자를 무효화하고 반환된 반복자를 사용합니다.
+			it = v.erase(it);
 		}
-		else
-		{
+		// 반복자는 else 부분에서만 증가함을 주목하십시오(왜?)
+		else {
 			++it;
 		}
-
-		for (int idx = 0; idx < v.size(); idx++)
-		{
-			printf("%d", v[idx]);
-		}
-		printf("\n");
 	}
+
+	for (int const& i : v) {
+		std::cout << i << ' ';
+	}
+
+	return 0;
 }
 #endif
