b48215e453c802f37e4f54a799bbb1eeeb5a092a
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-11-13 23:47:53 +0900

    2022.11.13_vector

--- Coding_Test/vector.cpp
+++ Coding_Test/vector.cpp
@@ -27,6 +27,33 @@ int main(void) {
 
 	printf("%f\n", value);
 
+	v = sqrt(Vector1.x * Vector1.x + Vector1.y * Vector1.y + Vector1.z * Vector1.z);
+	Vector1.x /= v;
+	Vector1.y /= v;
+	Vector1.z /= v;
+
+	v = sqrt(Vector2.x * Vector2.x + Vector2.y * Vector2.y + Vector2.z * Vector2.z);
+	Vector2.x /= v;
+	Vector2.y /= v;
+	Vector2.z /= v;
+
+	///< 내적하기
+	float theta;
+	float degree;
+
+	theta = Vector1.x * Vector2.x + Vector1.y * Vector2.y + Vector1.z * Vector2.z;
+
+	theta = acos(theta);
+
+	degree = theta * (180 / 3.141592);
+
+	float n_x, n_y, n_z;
+
+	n_x = Vector1.y * Vector2.z - Vector1.z * Vector2.y;
+	n_y = Vector1.z * Vector2.x - Vector1.x * Vector2.z;
+	n_z = Vector1.x * Vector2.y - Vector1.y * Vector2.x;
+
+
 	return 0;
 
 }
