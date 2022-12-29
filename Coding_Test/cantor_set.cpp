92d0bfd66b87142f1eba62b0aab23a69f7dbe0a2
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-12-29 19:50:58 +0900

    2022.12.29_cantor_set

---
+++ Coding_Test/cantor_set.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int solution(int n, long long l, long long r) {
+    int answer = 0;
+
+    vector<bitset<5>> cantor_vector;
+    string cantor_number = "1";
+    string number_one = "11011";
+    string number_zero = "00000";
+
+    bitset<5> bit(number_one);
+
+    for (int i = 0; i < bit.size(); i++)
+        if (bit[i] == 1) cout << bit[i];
+
+    int cnt = 1;
+
+    //     while(cnt <= n)
+    //     {
+    //         string temp_cantor = "";
+    //         for(long long idx = 0; idx < cantor.length(); idx++)
+    //         {
+    //             if(cantor[idx] == '1')
+    //                 temp_cantor += number_one;
+    //             else
+    //                 temp_cantor += number_zero;
+    //         }
+
+    //         cantor = temp_cantor;
+    //         cnt++;
+    //     }
+
+    //     for(long long idx = l - 1; idx < r; idx++)
+    //     {
+    //         if(cantor[idx] == '1')
+    //             answer++;
+    //     }
+
+    return answer;
+}
