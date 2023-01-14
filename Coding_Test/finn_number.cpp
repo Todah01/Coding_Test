4f4b4073249bfaf97b7f7fcb237ff2deaced81dd
Todah01 <57752105+Todah01@users.noreply.github.com>
2023-01-14 14:56:52 +0900

    2023.01.14_fibo_by_matrix

--- Coding_Test/finn_number.cpp
+++ Coding_Test/finn_number.cpp
@@ -52,3 +52,20 @@ int solution(int n) {
 }
 #endif
 #pragma endregion
+
+#if 0
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> fibo_vector = { 0, 1 };
+
+    for (int i = 2; i < n + 1; i++)
+        fibo_vector.push_back((fibo_vector[i - 1] + fibo_vector[i - 2]) % 1234567);
+
+    return fibo_vector[n] % 1234567;
+}
+#endif
