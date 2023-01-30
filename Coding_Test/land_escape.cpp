88d0a1135317ba668989ede4007639340018dd66
Todah01 <57752105+Todah01@users.noreply.github.com>
2023-01-30 18:49:39 +0900

    2023.01.30_cutting_arr

---
+++ Coding_Test/land_escape.cpp
@@ -0,0 +1,27 @@
+#if 0
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    for (int i = 1; i < land.size(); i++)
+        for (int j = 0; j < 4; j++)
+        {
+            int temp = land[i - 1][j];
+            land[i - 1][j] = -1;
+            land[i][j] += max({ land[i - 1][0],land[i - 1][1],land[i - 1][2],land[i - 1][3] });
+            land[i - 1][j] = temp;
+        }
+
+    return *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
+}
+#endif
+
+#pragma region Using cutting arr
+#if 0
+
+#endif
+#pragma endregion
--- Coding_Test/pair_number.cpp
+++ Coding_Test/pair_number.cpp
@@ -85,17 +85,26 @@ using namespace std;
 string solution(string X, string Y)
 {
     string answer = "";
+
     map<char, int> mx;
     for (char c : X) mx[c]++;
+
     map<char, int> my;
     for (char c : Y) my[c]++;
+
     for (int i = 9; i >= 0; i--)
     {
         int num = min(mx[i + '0'], my[i + '0']);
-        for (int j = 0; j < num; j++) answer += i + '0';
+
+        for (int j = 0; j < num; j++)
+            answer += i + '0';
     }
-    if (answer == "") answer = "-1";
-    else if (answer[0] == '0' && answer.size() > 1) answer = "0";
+
+    if (answer == "")
+        answer = "-1";
+    else if (answer[0] == '0' && answer.size() > 1)
+        answer = "0";
+
     return answer;
 }
 #endif
