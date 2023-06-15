51c395e27574603fc79b3be1e2148311d1366e32
Todah01 <wobo5695@gmail.com>
2023-06-15 20:41:16 +0900

    2023.06.15_bfs

--- Coding_Test/travel_island.cpp
+++ Coding_Test/travel_island.cpp
@@ -1,26 +1,49 @@
 #if 0
-from collections import deque
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
 
-def solution(maps) :
-    answer = []
-    row, col = len(maps), len(maps[0])
-    visited = [[0 for c in range(col)]for r in range(row)]
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, -1, 1]
+using namespace std;
 
-    visited[0][0] = 1
-    QUEUE = [(0, 0)]
+vector<int> solution(vector<string> maps) {
+    vector<int> answer;
+    int row = maps.size();
+    int col = maps[0].length();
+    vector<int> dx = { 1, -1, 0, 0 };
+    vector<int> dy = { 0, 0, -1, 1 };
+    vector<int> col_visited(col, 0);
+    vector<vector<int>> visited(row, col_visited);
 
-    for r in range(row) :
-        for c in range(col) :
+    for (int r = 0; r < row; r++)
+        for (int c = 0; c < col; c++)
+            if (maps[r][c] != 'X' && visited[r][c] != 1) {
+                queue<pair<int, int>> Q;
+                Q.push({ r, c });
+                visited[r][c] = 1;
+                int food = maps[r][c] - '0';
 
+                while (!Q.empty()) {
+                    int cr = Q.front().first;
+                    int cc = Q.front().second;
+                    Q.pop();
 
-            while QUEUE :
-                cv, cr = QUEUE.pop()
-
-                for idx in range(4) :
-                    dr = cr + dx[idx]
-                    dc = cc + dy[idx]
-                    if dr >= 0 and dr < rowand dc >= 0 and dc < coland maps[dr][dc] != 0 and visited[dr][dc] != 1 :
-                        QUEUE.append((dr, dc))
+                    for (int idx = 0; idx < 4; idx++) {
+                        int dr = cr + dx[idx];
+                        int dc = cc + dy[idx];
+                        if (dr >= 0 && dr < row && dc >= 0 && dc < col && maps[dr][dc] != 'X') {
+                            if (!visited[dr][dc]) {
+                                visited[dr][dc] = 1;
+                                food += maps[dr][dc] - '0';
+                                Q.push({ dr, dc });
+                            }
+                        }
+                    }
+                }
+                answer.push_back(food);
+            }
+    sort(answer.begin(), answer.end(), less<>());
+    if (answer.empty()) answer.push_back(-1);
+    return answer;
+}
 #endif
