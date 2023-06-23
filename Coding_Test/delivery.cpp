56b020017d3eb8b82fc763d5886f347e3cfc61d4
Todah01 <wobo5695@gmail.com>
2023-06-23 19:59:44 +0900

    2023.06.23_floyd_warshall

--- Coding_Test/delivery.cpp
+++ Coding_Test/delivery.cpp
@@ -1,47 +1,94 @@
 #if 0
-from collections import deque, defaultdict
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 
-def create_graph(data) :
-    graph = {}
+using namespace std;
 
-    for edge in data :
-node_a, node_b, distance = edge
+vector<vector<int>> create_graph(vector<vector<int>> data, int n) {
+    vector<int> temp_graph(n + 1, 500001);
+    vector<vector<int>> graph(n + 1, temp_graph);
 
-if node_a not in graph :
-graph[node_a] = {}
-graph[node_a][node_b] = distance
+    for (int i = 0; i < n + 1; i++)
+        graph[i][i] = 0;
 
-if node_b not in graph :
-graph[node_b] = {}
-graph[node_b][node_a] = distance
+    for (auto iter : data) {
+        graph[iter[0]][iter[1]] = min(graph[iter[0]][iter[1]], iter[2]);
+        graph[iter[1]][iter[0]] = min(graph[iter[1]][iter[0]], iter[2]);
+    }
 
-return graph
+    return graph;
+}
 
-def bfs(graph) :
-    q = deque()
-    visited_dist = defaultdict(int)
-    start_node = 1
-    dist = 0
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+    vector<vector<int>> graph;
+    graph = create_graph(road, N);
 
-    visited_dist[start_node] = dist
-    q.append(start_node)
+    for (int k = 1; k < N + 1; k++) {
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < N + 1; j++) {
+                graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j]);
+            }
+        }
+    }
 
-    while q:
-node = q.popleft()
-neighbors = graph[node]
-print(visited_dist)
-for neighbor in neighbors :
-if neighbor not in visited_dist :
-dist += neighbors[neighbor]
-visited_dist[neighbor] = dist
-q.append(neighbor)
+    for (auto iter : graph[1]) {
+        if (iter <= K) answer++;
+    }
 
-return 0
+    return answer;
+}
+#endif
 
+// DFS VER.
+#if 0
+#include <iostream>
+#include <vector>
 
-def solution(N, road, K) :
-    answer = 0
-    graph = create_graph(road)
-    bfs(graph)
-    return answer
+using namespace std;
+
+int     hour[51][51] = { 0, };
+bool    possible[51] = { 0, };
+
+void    recurs(int before, int t, int K, bool* visit)
+{
+    if (t > K)
+        return;
+    possible[before] = true;
+    for (int i = 2; i < 51; i++)
+    {
+        if (!visit[i] && hour[before][i])
+        {
+            visit[i] = true;
+            recurs(i, t + hour[before][i], K, visit);
+            visit[i] = false;
+        }
+    }
+}
+
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+
+    for (int i = 0; i < road.size(); i++)
+    {
+        int a = road[i][0], b = road[i][1], t = road[i][2];
+        if (t < hour[a][b] || hour[a][b] == 0)
+        {
+            hour[a][b] = t;
+            hour[b][a] = t;
+        }
+    }
+    bool    visit[51] = { 0, };
+    visit[1] = true;
+    recurs(1, 0, K, visit);
+    for (int i = 1; i < 51; i++)
+    {
+        if (possible[i] == true)
+            answer++;
+    }
+
+    return answer;
+}
 #endif
