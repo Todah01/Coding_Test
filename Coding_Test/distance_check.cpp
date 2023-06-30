e3cb96078fb0ffc7ca8f9b509159e2231c49e4d5
Todah01 <wobo5695@gmail.com>
2023-06-30 17:58:29 +0900

    2023.06.30_bfs_to_mahatten_distance

--- Coding_Test/distance_check.cpp
+++ Coding_Test/distance_check.cpp
@@ -1,33 +1,51 @@
 #if 0
-def solution(places) :
-    answer = []
-    for green_room in places :
-seat_coods = []
-Check_correct = True
-
-# Get coodinate of seat from each room
-for seat_guide_idx in range(len(green_room)) :
-    for seat_info_idx in range(len(green_room[seat_guide_idx])) :
-        if green_room[seat_guide_idx][seat_info_idx] == 'P' :
-            seat_coods.append([seat_guide_idx, seat_info_idx])
-
-            # Check Manhattan distance
-            for idx in range(len(seat_coods)) :
-                temp_coods = seat_coods[idx + 1:]
-                for seat_cood in temp_coods :
-Manhatten_dist = abs(seat_coods[idx][0] - seat_cood[0]) + abs(seat_coods[idx][1] - seat_cood[1])
-if Manhatten_dist == 2 :
-    for x in range(min(seat_coods[idx][0], seat_cood[0]), max(seat_coods[idx][0], seat_cood[0] + 1)) :
-        for y in range(min(seat_coods[idx][1], seat_cood[1]), max(seat_coods[idx][1], seat_cood[1] + 1)) :
-            if green_room[x][y] == 'O' :
-                Check_correct = False
-                break
-                elif Manhatten_dist == 1 :
-                Check_correct = False
-                break
-
-                if Check_correct : answer.append(1)
-                else : answer.append(0)
-
-                    return answer
+#include <vector>
+#include <string>
+using namespace std;
+int check(vector<string>& place) {
+    vector<pair<int, int>> persons;
+    for (int x = 0, y; x < place.size(); ++x)
+        for (y = 0; y < place[x].size(); ++y)
+            if (place[x].at(y) == 'P')               persons.push_back(make_pair(x, y));
+    if (persons.size() <= 1)
+        return 1;
+    for (int x = 0, y; x < persons.size() - 1; ++x) {
+        for (y = x + 1; y < persons.size(); ++y) {
+            auto& t1 = persons[x];
+            auto& t2 = persons[y];
+            const auto& distance = abs(t1.first - t2.first) + abs(t1.second - t2.second);
+            if (distance == 1)
+                return 0;
+            if (distance == 2) {
+                if (t1.first == t2.first) {           // 좌/우 로 앉아 있는 경우
+                    if (place[t1.first].at(min(t1.second, t2.second) + 1) != 'X')
+                        return 0;
+                }
+                else if (t1.second == t2.second) {    // 상/하 로 앉아 있는 경우
+                    if (place[min(t1.first, t2.first) + 1].at(t1.second) != 'X')
+                        return 0;
+                }
+                else {                                // 대각으로 앉아 있는 경우
+                    if ((t1.first < t2.first && t1.second < t2.second) || (t2.first < t1.first && t2.second < t1.second)) {
+                        if (place[min(t1.first, t2.first)].at(max(t1.second, t2.second)) != 'X' ||
+                            place[max(t1.first, t2.first)].at(min(t1.second, t2.second)) != 'X')
+                            return 0;
+                    }
+                    else {
+                        if (place[min(t1.first, t2.first)].at(min(t1.second, t2.second)) != 'X' ||
+                            place[max(t1.first, t2.first)].at(max(t1.second, t2.second)) != 'X')
+                            return 0;
+                    }
+                }
+            }
+        }
+    }
+    return 1;
+}
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> ans;
+    for (int t = 0, x, y; t < places.size(); ++t)
+        ans.push_back(check(places[t]));
+    return ans;
+}
 #endif
