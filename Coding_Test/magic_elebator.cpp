d1a3dd2082a67498c73fc2329bee2c42fece7cbc
Todah01 <wobo5695@gmail.com>
2023-07-03 21:19:04 +0900

    2023.07.03_bfs_dfs_to_arr

--- Coding_Test/magic_elebator.cpp
+++ Coding_Test/magic_elebator.cpp
@@ -1,19 +1,33 @@
 #if 0
-def solution(storey) :
-    answer = 0
-    while storey > 0:
-storey, remainder = divmod(storey, 10)
-if remainder < 5 :
-    answer += remainder
-    elif remainder > 5 :
-    answer += 10 - remainder
-    storey += 1
-    elif remainder == 5 :
-    if storey % 10 < 5 :
-        answer += remainder
-        elif storey % 10 >= 5 :
-        answer += 10 - remainder
-        storey += 1
+#include <string>
+#include <vector>
+#include <iostream>
 
-        return answer
+using namespace std;
+
+int solution(int storey) {
+    int answer = 0;
+    while (storey > 0) {
+        int remainder = storey % 10;
+        storey /= 10;
+
+        if (remainder < 5) {
+            answer += remainder;
+        }
+        else if (remainder > 5) {
+            answer += 10 - remainder;
+            storey += 1;
+        }
+        else if (remainder == 5) {
+            if (storey % 10 < 5) {
+                answer += remainder;
+            }
+            else if (storey % 10 >= 5) {
+                answer += 10 - remainder;
+                storey += 1;
+            }
+        }
+    }
+    return answer;
+}
 #endif
