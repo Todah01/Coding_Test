3968b8a415c11edcdb2032cdfaf31022703af383
Todah01 <wobo5695@gmail.com>
2023-07-09 16:09:37 +0900

    2023.07.09_maze_exit

--- Coding_Test/maze_exit.cpp
+++ Coding_Test/maze_exit.cpp
@@ -1,39 +1,68 @@
 #if 0
-from collections import deque
+#include <string>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <limits.h>
 
-def bfs(maps, arive) :
-    r, c = len(maps), len(maps[0])
-    visited = [[0 if maps[i][j] != 'X' else 'X' for j in range(c)]for i in range(r)]
-    sp = [[i, j]for i in range(r) for j in range(c) if maps[i][j] == 'S']
-    lp = [[i, j]for i in range(r) for j in range(c) if maps[i][j] == 'L']
-    q = deque()
+using namespace std;
 
-    if arive == 'E': q.append(lp[0])
-        elif arive == 'L' : q.append(sp[0])
+int bfs(vector<string> maps, tuple<int, int> start, tuple<int, int> exit, int row, int col) {
+    int start_r, start_c, exit_r, exit_c;
+    tie(start_r, start_c) = start;
+    tie(exit_r, exit_c) = exit;
 
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, -1, 1]
+    vector<vector<int>> visited(row, vector<int>(col, INT_MAX));
+    visited[start_r][start_c] = 0;
 
-        while q :
-            cr, cc = q.popleft()
-            for idx in range(4) :
-                dr = cr + dx[idx]
-                dc = cc + dy[idx]
-                if 0 <= dr < r and 0 <= dc < cand visited[dr][dc] != 'X' :
-                    if maps[dr][dc] == arive :
-                        visited[dr][dc] = visited[cr][cc] + 1
-                        return visited[dr][dc]
-                    else :
-                        visited[dr][dc] = visited[cr][cc] + 1
-                        q.append([dr, dc])
-                        return 0
+    queue<tuple<int, int, int>> q;
+    q.emplace(start_r, start_c, visited[start_r][start_c]);
 
-                        def solution(maps) :
-                        s_to_l_length = bfs(maps, 'L')
-                        l_to_e_length = bfs(maps, 'E')
+    vector<vector<int>> dir = { {1, 0}, {-1, 0}, {0, -1}, {0, 1} };
 
-                        if not s_to_l_length or not l_to_e_length :
-                            return -1
-                        else:
-return s_to_l_length + l_to_e_length
+    while (!q.empty()) {
+        int cr, cc, cvc;
+        tie(cr, cc, cvc) = q.front();
+        q.pop();
+
+        if (cr == exit_r && cc == exit_c)
+            return cvc;
+
+        for (auto iter : dir) {
+            int dr = cr + iter[0], dc = cc + iter[1], dvc = cvc + 1;
+            if (dr >= 0 && dr < row && dc >= 0 && dc < col && maps[dr][dc] != 'X' && dvc < visited[dr][dc]) {
+                visited[dr][dc] = dvc;
+                q.emplace(dr, dc, dvc);
+            }
+        }
+    }
+
+    return -1;
+}
+
+int solution(vector<string> maps) {
+    int row = maps.size(), col = maps[0].length();
+    tuple<int, int> start(0, 0), lever(0, 0), exit(0, 0);
+    for (int r = 0; r < row; r++)
+        for (int c = 0; c < col; c++) {
+            if (maps[r][c] == 'S') {
+                get<0>(start) = r;
+                get<1>(start) = c;
+            }
+            else if (maps[r][c] == 'L') {
+                get<0>(lever) = r;
+                get<1>(lever) = c;
+            }
+            else if (maps[r][c] == 'E') {
+                get<0>(exit) = r;
+                get<1>(exit) = c;
+            }
+        }
+
+    int start_to_lever = bfs(maps, start, lever, row, col);
+    int lever_to_exit = bfs(maps, lever, exit, row, col);
+
+    if (start_to_lever != -1 && lever_to_exit != -1) return start_to_lever + lever_to_exit;
+    else return -1;
+}
 #endif
