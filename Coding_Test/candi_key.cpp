614869539ecb831f862832fbc301031959893440
Todah01 <wobo5695@gmail.com>
2023-07-11 23:08:43 +0900

    2023.07.11_candidate_key_none_of_includes

--- Coding_Test/candi_key.cpp
+++ Coding_Test/candi_key.cpp
@@ -1,22 +1,73 @@
 #if 0
-from itertools import combinations
-from collections import Counter
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <iostream>
 
-def solution(relation) :
-    answer = 0
-    data_list = [[]for i in range(len(relation[0]))]
+using namespace std;
 
-    for idx in range(len(relation[0])) :
-        for info in relation :
-data_list[idx].append(info[idx])
+vector<vector<int>> get_subset(vector<int> iterable) {
+    vector<vector<int>> result;
+    int len = iterable.size();
 
-exclude_list = []
-for r in range(1, len(data_list) + 1) :
-    idx_list = combinations([i for i in range(len(data_list))], r);
-for idx in idx_list :
-check_dup = Counter(["".join(elements) for elements in zip(*comb)])
-if not any(count > 1 for count in check_dup.values()) :
-    answer += 1
+    for (int i = 1; i < len + 1; i++) {
+        vector<bool> v(len - i, false);
+        v.insert(v.end(), i, true);
+        do {
+            vector<int> temp;
+            for (int k = 0; k < len; k++) {
+                if (v[k]) temp.push_back(iterable[k]);
+            }
+            result.push_back(temp);
+        } while (next_permutation(v.begin(), v.end()));
+    }
 
-    return answer
+    return result;
+}
+
+int solution(vector<vector<string>> relation) {
+    int answer = 0;
+    int row = relation.size(), col = relation[0].size();
+
+    vector<int> attribute_cnt;
+    for (int i = 0; i < col; i++)
+        attribute_cnt.push_back(i);
+
+    vector<vector<int>> sub_list = get_subset(attribute_cnt);
+    vector<vector<int>> uniq_list;
+
+    for (auto sub : sub_list) {
+        bool unique = true;
+        set<string> key_set;
+        for (int i = 0; i < row; i++) {
+            string key = "";
+            for (auto j : sub)
+                key += relation[i][j];
+            if (key_set.find(key) != key_set.end()) {
+                unique = false;
+                break;
+            }
+            key_set.insert(key);
+        }
+        if (unique)
+            uniq_list.push_back(sub);
+    }
+
+    reverse(uniq_list.begin(), uniq_list.end());
+
+    for (int i = 0; i < uniq_list.size(); i++) {
+        bool unique = true;
+        for (int j = i + 1; j < uniq_list.size(); j++) {
+            if (includes(uniq_list[i].begin(), uniq_list[i].end(), uniq_list[j].begin(), uniq_list[j].end())) {
+                unique = false;
+                break;
+            }
+        }
+
+        if (unique) answer++;
+    }
+
+    return answer;
+}
 #endif
