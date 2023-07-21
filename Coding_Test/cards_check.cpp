957ab6a35e8a1a4063aaecd50d7a7d553a3057a0
Todah01 <wobo5695@gmail.com>
2023-07-21 23:00:38 +0900

    2023.07.21_two_pointer

---
+++ Coding_Test/cards_check.cpp
@@ -0,0 +1,25 @@
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    int l = 0;
+    int r = 0;
+
+    for (string& g : goal) {
+        if (g == cards1[l]) {
+            l++;
+        }
+        else if (g == cards2[r]) {
+            r++;
+        }
+        else {
+            return "No";
+        }
+    }
+
+    return "Yes";
+}
+#endif
