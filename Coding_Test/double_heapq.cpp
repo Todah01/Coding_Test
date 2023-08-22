345ebb53dbd8bf8047f18cf220f9e440cc73d4eb
Todah01 <wobo5695@gmail.com>
2023-08-22 22:40:47 +0900

    2023.08.22_double_heap_I/O_dev

---
+++ Coding_Test/double_heapq.cpp
@@ -0,0 +1,31 @@
+#if 0
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> arguments) {
+    multiset<int> que;
+    multiset<int>::iterator iter;
+    string sub;
+
+    for (auto s : arguments) {
+        sub = s.substr(0, 2);
+        if (sub == "I ")
+            que.insert(stoi(s.substr(2, s.length() - 2)));
+        else if (s.substr(2, 1) == "1" && que.size() > 0)
+            que.erase(que.end());
+        else if (que.size() > 0)
+            que.erase(que.begin());
+    }
+
+    if (que.size() == 0) {
+        return { 0, 0 };
+    }
+    else {
+        iter = que.end(); answer.push_back(*iter);
+        iter = que.begin(); answer.push_back(*iter);
+    }
+}
+#endif
