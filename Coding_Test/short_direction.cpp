56afe5d6f034c49ec40667f79e53a8b88bf94ae9
Todah01 <wobo5695@gmail.com>
2023-09-01 15:04:35 +0900

    2023.09.01_fix_error_of_segmentation

--- Coding_Test/short_direction.cpp
+++ Coding_Test/short_direction.cpp
@@ -7,39 +7,43 @@ using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
-    vector<vector<int>> dp(n, vector<int>(m, 0));
-    for (auto puddle : puddles) {
-        dp[puddle[0] - 1][puddle[1] - 1] = 'X';
+    int dp[n][m];
+    fill(dp[0], dp[n], 0);
+
+    for (vector<int> puddle : puddles) {
+        dp[puddle[1] - 1][puddle[0] - 1] = -1;
     }
 
-    for (int i = 0; i < m; i++) {
-        if (dp[0][i] != 'X')
-            dp[0][i] = 1;
+    for (int i = 0; i < n; i++) {
+        if (dp[i][0] != -1)
+            dp[i][0] = 1;
         else
             break;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (dp[i][0] != 'X')
-            dp[i][0] = 1;
+    for (int i = 0; i < m; i++) {
+        if (dp[0][i] != -1)
+            dp[0][i] = 1;
         else
             break;
     }
 
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-            if (dp[i][j] != 'X' && dp[i][j - 1] == 'X') {
+            if (dp[i][j] != -1 && dp[i][j - 1] == -1) {
                 dp[i][j] = dp[i - 1][j];
             }
-            else if (dp[i][j] != 'X' && dp[i - 1][j] == 'X') {
+            else if (dp[i][j] != -1 && dp[i - 1][j] == -1) {
                 dp[i][j] = dp[i][j - 1];
             }
-            else if (dp[i][j] != 'X') {
+            else if (dp[i][j] != -1) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
+
+            dp[i][j] %= 1000000007;
         }
     }
 
-    return dp[n - 1][m - 1] % 1000000007;
+    return dp[n - 1][m - 1];
 }
 #endif
